6b7f7e90b92e6c983a3522ee69163632e8392de0
Sarah Alzahrani <86152151+s43sa@users.noreply.github.com>
2023-06-11 15:26:51 +0300

    Add files via upload

---
+++ db.py
@@ -0,0 +1,26 @@
+################################### run this code in MYSQworkbanch  to create the Data base #########################################
+#####################################################################################################################################
+
+
+#CREATE DATABASE IF NOT EXISTS `Dalili` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+#USE `Dalili`;
+
+
+#CREATE TABLE  IF NOT EXISTS `Developer` (
+#	`Id` int NOT NULL PRIMARY KEY,
+#  	`Fname` varchar(50) NOT NULL,
+ #   `Lname` varchar(50) ,
+  #	`Password` varchar(255) NOT NULL
+#);
+
+#INSERT INTO `Developer` (`Id`, `Fname`, `Lname`, `Password`) VALUES (1808251,'Deena','Almadani','Aa12345');
+
+
+ #CREATE TABLE IF NOT EXISTS `Students` (
+#	`email` varchar(60) NOT NULL ,
+ # 	`Fname` varchar(50) NOT NULL,
+  #  `Lname` varchar(50) ,
+  #	`password` varchar(255) NOT NULL,
+#  PRIMARY KEY (`email`)
+# ) ;
+
